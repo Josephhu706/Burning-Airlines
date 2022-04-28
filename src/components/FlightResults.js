083a2f883ce80e41919750ddc03484795380bbc9
kendlc <kennethcdlc@gmail.com>
2022-04-28 13:34:30 +1000

    pulling flight page

--- src/components/FlightResults.js
+++ src/components/FlightResults.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+
 
 const FlightResults = (props) => {
-    console.log(props)
   return (
-<Router>
+
     <div>
         <h1>{props.FlightResults}</h1>
         <h2>Flight Search Results ({ props.flights.length })</h2>
@@ -21,7 +20,7 @@ const FlightResults = (props) => {
                 <tr key={ flight.id }> 
                     <td> { flight.date }</td>
                     <td>{flight.id}</td>
-                    <Link to="/flight"><td> { flight.id }</td></Link>
+                    <td> { flight.id }</td>
                     <td> { flight.origin + ' > ' + flight.destination }</td>
                     <td> { flight.airplane_id }</td>
                 </tr> 
@@ -29,7 +28,7 @@ const FlightResults = (props) => {
             </tbody>
         </table>
     </div>
-</Router>
+
   );
 };
 
